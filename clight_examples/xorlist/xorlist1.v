f670badea1c433826ed5ff3c4372cbc8db35313f
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2023-11-16 14:41:38 +0900

    update xorlist spec

--- clight_examples/xorlist/xorlist1.v
+++ clight_examples/xorlist/xorlist1.v
@@ -47,66 +47,45 @@ Section PROP.
      (*     v                                 v   *)
      (* xs  - - - - - - - - - - - - - - - - - -   *)
 
-  Fixpoint frag_xorlist (q: Qp) (p_hd_prev p_hd p_tl p_tl_next: val) (xs : list val) {struct xs} : iProp :=
+  Fixpoint frag_xorlist (q: Qp) (hd_prev hd tl tl_next: ptrofs) (xs : list val) {struct xs} : iProp :=
     match xs with
-    | [] => ⌜p_hd_prev = p_tl /\ p_hd = p_tl_next⌝
+    | [] => ⌜hd_prev = tl /\ hd = tl_next⌝
     | Vlong a :: xs' =>
-      if Val.eq Vnullptr p_hd_prev
-      then
-        ∃ p_next m_hd i_next,
-          p_hd ↦m_hd#q≻ (encode_val Mint64 (Vlong a) ++ encode_val Mptr (Vptrofs (Ptrofs.repr i_next)))
-          ** p_hd ⊨m_hd# 0
-          ** live_ q # (m_hd, Dynamic)
-          ** ⌜m_hd.(sz) = (size_chunk Mint64 + size_chunk Mptr)%Z⌝
-          ** (if Z.eqb i_next 0 then ⌜p_next = Vnullptr⌝
-              else ∃ m_next, p_next (≃_ m_next) i_next)
-          ** frag_xorlist q p_hd p_next p_tl p_tl_next xs'
-      else
-        ∃ p_next m_prev m_hd i_prev i_key,
-          p_hd ↦m_hd#q≻ (encode_val Mint64 (Vlong a) ++ encode_val Mptr (Vptrofs (Ptrofs.repr i_key)))
-          ** p_hd ⊨m_hd# 0
-          ** live_ q # (m_hd, Dynamic)
-          ** ⌜m_hd.(sz) = (size_chunk Mint64 + size_chunk Mptr)%Z⌝
-          ** p_hd_prev (≃_ m_prev) i_prev
-          ** (if Z.eqb (Z.lxor i_prev i_key) 0 then ⌜p_next = Vnullptr⌝
-              else ∃ m_next, p_next (≃_ m_next) (Z.lxor i_prev i_key))
-          ** frag_xorlist q p_hd p_next p_tl p_tl_next xs'
+        ∃ m_hd hd_next,
+          ⌜m_hd.(sz) = (size_chunk Mint64 + size_chunk Mptr)%Z⌝
+          ** (Vptrofs hd) (⊨_m_hd,Dynamic,q) Ptrofs.zero
+          ** (Vptrofs hd) (↦_m_hd,q) (encode_val Mint64 (Vlong a) ++ encode_val Mptr (Vptrofs (Ptrofs.xor hd_prev hd_next)))
+          ** frag_xorlist q hd hd_next tl tl_next xs'
     | _ => False
     end%I.
 
-  Lemma unfold_frag_xorlist (q: Qp) (p_hd_prev p_hd p_tl p_tl_next: val) (xs : list val) :
-  frag_xorlist q p_hd_prev p_hd p_tl p_tl_next xs =
+  Lemma unfold_frag_xorlist (q: Qp) (hd_prev hd tl tl_next: ptrofs) (xs : list val) :
+  frag_xorlist q hd_prev hd tl tl_next xs =
     match xs with
-    | [] => ⌜p_hd_prev = p_tl /\ p_hd = p_tl_next⌝
+    | [] => ⌜hd_prev = tl /\ hd = tl_next⌝
     | Vlong a :: xs' =>
-      if Val.eq Vnullptr p_hd_prev
-      then
-        ∃ p_next m_hd i_next,
-          p_hd ↦m_hd#q≻ (encode_val Mint64 (Vlong a) ++ encode_val Mptr (Vptrofs (Ptrofs.repr i_next)))
-          ** p_hd ⊨m_hd# 0
-          ** live_ q # (m_hd, Dynamic) 
-          ** ⌜m_hd.(sz) = (size_chunk Mint64 + size_chunk Mptr)%Z⌝
-          ** (if Z.eqb i_next 0 then ⌜p_next = Vnullptr⌝
-              else ∃ m_next, p_next (≃_ m_next) i_next)
-          ** frag_xorlist q p_hd p_next p_tl p_tl_next xs'
-      else
-        ∃ p_next m_prev m_hd i_prev i_key,
-          p_hd ↦m_hd#q≻ (encode_val Mint64 (Vlong a) ++ encode_val Mptr (Vptrofs (Ptrofs.repr i_key)))
-          ** p_hd ⊨m_hd# 0
-          ** live_ q # (m_hd, Dynamic) 
-          ** ⌜m_hd.(sz) = (size_chunk Mint64 + size_chunk Mptr)%Z⌝
-          ** p_hd_prev (≃_ m_prev) i_prev
-          ** (if Z.eqb (Z.lxor i_prev i_key) 0 then ⌜p_next = Vnullptr⌝
-              else ∃ m_next, p_next (≃_ m_next) (Z.lxor i_prev i_key))
-          ** frag_xorlist q p_hd p_next p_tl p_tl_next xs'
+        ∃ m_hd hd_next,
+          ⌜m_hd.(sz) = (size_chunk Mint64 + size_chunk Mptr)%Z⌝
+          ** (Vptrofs hd) (⊨_m_hd,Dynamic,q) Ptrofs.zero
+          ** (Vptrofs hd) (↦_m_hd,q) (encode_val Mint64 (Vlong a) ++ encode_val Mptr (Vptrofs (Ptrofs.xor hd_prev hd_next)))
+          ** frag_xorlist q hd hd_next tl tl_next xs'
     | _ => False
     end%I.
   Proof. des_ifs. Qed.
 
-  Definition full_xorlist q p_hd p_tl xs : iProp := 
-    frag_xorlist q Vnullptr p_hd p_tl Vnullptr xs.
+  Definition full_xorlist q hd_hdl tl_hdl xs : iProp :=
+    (∃ m_hd_hdl m_tl_hdl m_hd m_tl p_hd p_tl i_hd i_tl ofs_hd_hdl ofs_tl_hdl tg_hd_hdl tg_tl_hdl,
+    hd_hdl (↦_m_hd_hdl,q) (encode_val Mptr p_hd)
+    ** hd_hdl (⊨_m_hd_hdl,tg_hd_hdl,q) ofs_hd_hdl
+    ** ⌜((size_chunk Mptr) | Ptrofs.unsigned ofs_hd_hdl)%Z⌝
+    ** p_hd (≃_m_hd) i_hd
+    ** tl_hdl (↦_m_tl_hdl,q) (encode_val Mptr p_tl)
+    ** tl_hdl (⊨_m_tl_hdl,tg_tl_hdl,q) ofs_tl_hdl
+    ** ⌜((size_chunk Mptr) | Ptrofs.unsigned ofs_tl_hdl)%Z⌝
+    ** p_tl (≃_m_tl) i_tl
+    ** frag_xorlist q Ptrofs.zero i_hd i_tl Ptrofs.zero xs)%I.
 
-  Example xorlist_example1 q p1 p2 p3 m1 m2 m3 i1 i2 i3:
+  (* Example xorlist_example1 q p1 p2 p3 m1 m2 m3 i1 i2 i3:
   (p1 ↦m1#q≻ (encode_val Mint64 (Vlong Int64.one) ++ encode_val Mint64 (Vptrofs (Ptrofs.repr i2)))
   ** p1 (≃_ m1) i1
   ** live_ q# (m1, Dynamic)
@@ -178,9 +157,9 @@ Section PROP.
     { iApply "n2_addr_b". }
     { rewrite Z.lxor_nilpotent. simpl. ss. }
     { iPureIntro. et. }
-  Qed.
+  Qed. *)
 
-  Example xorlist_example2 q p2 m2:
+  (* Example xorlist_example2 q p2 m2:
   p2 ↦m2#q≻ (encode_val Mint64 (Vlong Int64.one) ++ encode_val Mint64 (Vptrofs Ptrofs.zero))
   ** live_ q# (m2, Dynamic)
   ** p2 ⊨m2# 0
@@ -200,13 +179,13 @@ Section PROP.
     { simpl. iPureIntro. ss. }
     { simpl. iPureIntro. ss. }
     { simpl. iPureIntro. ss. }
-  Qed.
+  Qed. *)
     
   Lemma split_xorlist q p_hd_prev p_hd p_tl p_tl_next xs0 xs1
     : frag_xorlist q p_hd_prev p_hd p_tl p_tl_next (xs0 ++ xs1)
       ⊢ ∃ mid mid_next, frag_xorlist q p_hd_prev p_hd mid mid_next xs0 ** frag_xorlist q mid mid_next p_tl p_tl_next xs1.
   Proof.
-    revert q p_hd_prev p_hd p_tl p_tl_next xs1.
+    (* revert q p_hd_prev p_hd p_tl p_tl_next xs1.
     induction xs0; i; ss.
     - iIntros "A". iExists _,_. iSplit; et.
     - iIntros "A". des_ifs.
@@ -234,13 +213,14 @@ Section PROP.
           iExists _,_. iFrame.
           iExists _,_,_,_,_. iFrame. rewrite Heq.
           iExists _. iFrame.
-  Qed.
+  Qed. *)
+  Admitted.
 
   Lemma concat_xorlist q p_hd_prev p_hd p_tl p_tl_next mid mid_next xs0 xs1
     : frag_xorlist q p_hd_prev p_hd mid mid_next xs0 ** frag_xorlist q mid mid_next p_tl p_tl_next xs1
       ⊢ frag_xorlist q p_hd_prev p_hd p_tl p_tl_next (xs0 ++ xs1).
   Proof.
-    revert q p_hd_prev p_hd p_tl p_tl_next mid mid_next xs1.
+    (* revert q p_hd_prev p_hd p_tl p_tl_next mid mid_next xs1.
     induction xs0; i; ss.
     - iIntros "[% A]". des. clarify.
     - iIntros "A". des_ifs; try solve [iDestruct "A" as "[% A]"; clarify].
@@ -266,13 +246,14 @@ Section PROP.
         * iDestruct "RES" as (m_next) "NXT_CAP".
           iExists _,_,_,_,_. iFrame. rewrite Heq.
           iExists _. iFrame.
-  Qed.
+  Qed. *)
+  Admitted.
 
   Lemma rev_xorlist q p_hd_prev p_hd p_tl p_tl_next xs
     : frag_xorlist q p_hd_prev p_hd p_tl p_tl_next xs
       ⊢ frag_xorlist q p_tl_next p_tl p_hd p_hd_prev (rev xs).
   Proof.
-    revert q p_hd_prev p_hd p_tl p_tl_next.
+    (* revert q p_hd_prev p_hd p_tl p_tl_next.
     induction xs; i; ss.
     - iIntros "%". des. iPureIntro. clarify.
     - iIntros "A". destruct a; try solve [iDestruct "A" as "%"; clarify].
@@ -319,7 +300,8 @@ Section PROP.
           destruct (i_prev =? 0)%Z eqn: ?.
           { apply Z.eqb_eq in Heqb0. clarify. }
           iExists _. iFrame.
-  Qed.
+  Qed. *)
+  Admitted.
 
 End PROP.
 
@@ -331,38 +313,33 @@ Section SPEC.
   Context `{@GRA.inG blockaddressRA Σ}.
 
   Definition encrypt_hoare1 : _ -> ord * (Any.t -> iProp) * (Any.t -> iProp) :=
-      fun '(left_ptr, right_ptr, m_l, m_r, tg_l, tg_r, q_l, q_r) => (
+      fun '(left_ptr, right_ptr, m_l, m_r, ofs_l, ofs_r, tg_l, tg_r, q_l, q_r) => (
             (ord_pure 1%nat),
-            (fun varg => ∃ ofs_l ofs_r, ⌜varg = [left_ptr; right_ptr]↑⌝
-                         ** left_ptr ⊨m_l# ofs_l
-                         ** right_ptr ⊨m_r# ofs_r
-                         ** live_ q_l# (m_l, tg_l)
-                         ** live_ q_r# (m_r, tg_r)),
-            (fun vret => ∃ i_l i_r, ⌜vret = (Vptrofs (Ptrofs.repr (Z.lxor i_l i_r)))↑⌝
+            (fun varg => ⌜varg = [left_ptr; right_ptr]↑⌝
+                         ** left_ptr (⊨_m_l,tg_l,q_l) ofs_l
+                         ** right_ptr (⊨_m_r,tg_r,q_r) ofs_r),
+            (fun vret => ∃ i_l i_r, ⌜vret = (Vptrofs (Ptrofs.xor i_l i_r))↑⌝
+                         ** left_ptr (⊨_m_l,tg_l,q_l) ofs_l
+                         ** right_ptr (⊨_m_r,tg_r,q_r) ofs_r
                          ** left_ptr (≃_ m_l) i_l
-                         ** right_ptr (≃_ m_r) i_r
-                         ** live_ q_l# (m_l, tg_l)
-                         ** live_ q_r# (m_r, tg_r)))%I.
+                         ** right_ptr (≃_ m_r) i_r))%I.
 
   Definition encrypt_hoare2 : _ -> ord * (Any.t -> iProp) * (Any.t -> iProp) :=
-      fun '(left_ptr, m_l, tg_l, q_l) => (
+      fun '(left_ptr, m_l, ofs_l, tg_l, q_l) => (
             (ord_pure 1%nat),
-            (fun varg => ∃ ofs_l, ⌜varg = [left_ptr; Vnullptr]↑⌝
-                         ** left_ptr ⊨m_l# ofs_l
-                         ** live_ q_l# (m_l, tg_l)),
+            (fun varg => ⌜varg = [left_ptr; Vnullptr]↑⌝
+                         ** left_ptr (⊨_m_l,tg_l,q_l) ofs_l),
             (fun vret => ∃ i_l, ⌜vret = (Vptrofs (Ptrofs.repr i_l))↑⌝
-                         ** left_ptr (≃_ m_l) i_l
-                         ** live_ q_l# (m_l, tg_l)))%I.
+                         ** left_ptr (⊨_m_l,tg_l,q_l) ofs_l))%I.
 
   Definition encrypt_hoare3 : _ -> ord * (Any.t -> iProp) * (Any.t -> iProp) :=
-      fun '(right_ptr, m_r, tg_r, q_r) => (
+      fun '(right_ptr, m_r, ofs_r, tg_r, q_r) => (
             (ord_pure 1%nat),
-            (fun varg => ∃ ofs_r, ⌜varg = [Vnullptr; right_ptr]↑⌝
-                         ** right_ptr ⊨m_r# ofs_r
-                         ** live_ q_r# (m_r, tg_r)),
-            (fun vret => ∃ i_r, ⌜vret = (Vptrofs (Ptrofs.repr i_r))↑⌝
+            (fun varg => ⌜varg = [Vnullptr; right_ptr]↑⌝
+                         ** right_ptr (⊨_m_r,tg_r,q_r) ofs_r),
+            (fun vret => ∃ i_r, ⌜vret = (Vptrofs i_r)↑⌝
                          ** right_ptr (≃_ m_r) i_r
-                         ** live_ q_r# (m_r, tg_r)))%I.
+                         ** right_ptr (⊨_m_r,tg_r,q_r) ofs_r))%I.
 
   Definition encrypt_hoare4 : _ -> ord * (Any.t -> iProp) * (Any.t -> iProp) :=
       fun '() => (
@@ -379,19 +356,18 @@ Section SPEC.
     ).
 
   Definition decrypt_hoare1 : _ -> ord * (Any.t -> iProp) * (Any.t -> iProp) :=
-      fun '(i_key, ptr, m, q, tg) => (
+      fun '(i_key, ptr, m, ofs, q, tg) => (
         (ord_pure 1%nat),
-        (fun varg => ∃ key ofs, ⌜varg = [key; ptr]↑ /\ key = Vptrofs i_key⌝
-                     ** ptr ⊨m# ofs
-                     ** live_ q# (m, tg)),
-        (fun vret => ∃ i_ptr, ⌜vret = (Vptrofs (Ptrofs.xor i_key (Ptrofs.repr i_ptr)))↑⌝
-                     ** ptr (≃_m) i_ptr
-                     ** live_ q# (m, tg)))%I.
+        (fun varg => ⌜varg = [Vptrofs i_key; ptr]↑⌝
+                     ** ptr (⊨_m,tg,q) ofs),
+        (fun vret => ∃ i_ptr, ⌜vret = (Vptrofs (Ptrofs.xor i_key i_ptr))↑⌝
+                     ** ptr (⊨_m,tg,q) ofs
+                     ** ptr (≃_m) i_ptr))%I.
                      
   Definition decrypt_hoare2 : _ -> ord * (Any.t -> iProp) * (Any.t -> iProp) :=
       fun '(i_key) => (
         (ord_pure 1%nat),
-        (fun varg => ∃ key, ⌜varg = [key; Vnullptr]↑ /\ key = Vptrofs i_key⌝),
+        (fun varg => ⌜varg = [Vptrofs i_key; Vnullptr]↑⌝),
         (fun vret => ⌜vret = (Vptrofs i_key)↑⌝))%I.
   
   Definition decrypt_spec : fspec :=
@@ -411,26 +387,12 @@ Section SPEC.
 
   Definition add_spec : fspec :=
     (mk_simple
-      (fun '(hd_handler, tl_handler, m_h, m_t, item, at_tail, xs) => (
+      (fun '(hd_handler, tl_handler, item, at_tail, xs) => (
         (ord_pure 2%nat),
-        (fun varg => ∃ hd_old tl_old ofs_hd_old ofs_tl_old,
-                     ⌜varg = [hd_handler; tl_handler; Vlong item; Vint at_tail]↑
-                      /\ ((size_chunk Mptr) | ofs_hd_old)%Z
-                      /\ ((size_chunk Mptr) | ofs_tl_old)%Z⌝
-                     ** hd_handler ↦m_h#1≻ encode_val Mptr hd_old
-                     ** tl_handler ↦m_t#1≻ encode_val Mptr tl_old
-                     ** hd_handler ⊨m_h# ofs_hd_old
-                     ** tl_handler ⊨m_t# ofs_tl_old
-                     ** full_xorlist 1 hd_old tl_old xs),
-        (fun vret => ∃ hd_new tl_new ofs_hd_new ofs_tl_new,
-                     ⌜vret = Vundef↑
-                     /\ ((size_chunk Mptr) | ofs_hd_new)%Z
-                     /\ ((size_chunk Mptr) | ofs_tl_new)%Z⌝
-                     ** hd_handler ↦m_h#1≻ encode_val Mptr hd_new
-                     ** tl_handler ↦m_t#1≻ encode_val Mptr tl_new
-                     ** hd_handler ⊨m_h# ofs_hd_new
-                     ** tl_handler ⊨m_t# ofs_tl_new
-                     ** full_xorlist 1 hd_new tl_new (vlist_add (Vlong item) xs (Vint at_tail)))
+        (fun varg => ⌜varg = [hd_handler; tl_handler; Vlong item; Vint at_tail]↑⌝
+                     ** full_xorlist 1 hd_handler tl_handler xs),
+        (fun vret => ⌜vret = Vundef↑⌝
+                     ** full_xorlist 1 hd_handler tl_handler (vlist_add (Vlong item) xs (Vint at_tail)))
     )))%I.
 
   (* {hd_handler |-> hd  /\ tl_handler |-> tl /\ is_xorlist hd tl xs}
@@ -442,50 +404,35 @@ Section SPEC.
               else r = hd xs /\ exists new_tl, hd_handler |-> hd /\ tl_handler |-> new_tl /\ is_xorlist hd new_tl (tl xs)
      } *)
 
-  Definition null_or_int (p q: val) : iProp :=
-    (⌜p = q⌝ ∨ ∃ i, ⌜p = Vptrofs (Ptrofs.repr i)⌝ ** ∃ m, q (≃_m) i)%I.
-
   Definition delete_spec : fspec :=
     (mk_simple
-      (fun '(hd_handler, tl_handler, m_h, m_t, from_tail, xs) => (
+      (fun '(hd_handler, tl_handler, from_tail, xs) => (
         (ord_pure 2%nat),
-        (fun varg => ∃ hd_old tl_old ofs_hd_old ofs_tl_old,
-                     ⌜varg = [hd_handler; tl_handler; Vint from_tail]↑
-                     /\ ((size_chunk Mptr) | ofs_hd_old)%Z
-                     /\ ((size_chunk Mptr) | ofs_tl_old)%Z⌝
-                     ** hd_handler ↦m_h#1≻ encode_val Mptr hd_old
-                     ** tl_handler ↦m_t#1≻ encode_val Mptr tl_old
-                     ** hd_handler ⊨m_h# ofs_hd_old
-                     ** tl_handler ⊨m_t# ofs_tl_old
-                     ** full_xorlist 1 hd_old tl_old xs),
+        (fun varg => ⌜varg = [hd_handler; tl_handler; Vint from_tail]↑⌝
+                     ** full_xorlist 1 hd_handler tl_handler xs),
         (fun vret => let '(item, xs') := vlist_delete xs (Vint from_tail) (Vlong Int64.zero) in
-                     ∃ hd_new_data tl_new_data hd_new tl_new ofs_hd_new ofs_tl_new,
-                     ⌜vret = item↑
-                     /\ ((size_chunk Mptr) | ofs_hd_new)%Z
-                     /\ ((size_chunk Mptr) | ofs_tl_new)%Z⌝
-                     ** hd_handler ↦m_h#1≻ encode_val Mptr hd_new_data
-                     ** tl_handler ↦m_t#1≻ encode_val Mptr tl_new_data
-                     ** hd_handler ⊨m_h# ofs_hd_new
-                     ** tl_handler ⊨m_t# ofs_tl_new
-                     ** null_or_int hd_new_data hd_new 
-                     ** null_or_int tl_new_data tl_new 
-                     ** full_xorlist 1 hd_new tl_new xs')
+                     ⌜vret = item↑⌝ ** full_xorlist 1 hd_handler tl_handler xs')
     )))%I.
 
   Definition search_spec : fspec :=
     (mk_simple
-      (fun '(hd_handler, tl_handler, from_tail, index, idx, xs, m_h, m_t, q, q_h, q_t) => (
+      (fun '(hd_handler, tl_handler, from_tail, index, xs, idx, q) => (
         (ord_pure 2%nat),
-        (fun varg => ∃ hd tl, ⌜varg = [hd_handler; tl_handler; Vint from_tail; Vlong index]↑
+        (fun varg => ⌜varg = [hd_handler; tl_handler; Vint from_tail; Vlong index]↑
                      /\ check_inbound xs (Vint from_tail) (Vlong index) = Some idx⌝
-                     ** hd_handler ↦m_h#q_h≻ encode_val Mptr hd
-                     ** tl_handler ↦m_t#q_t≻ encode_val Mptr tl
-                     ** full_xorlist q hd tl xs),
-        (fun vret => ∃ hd tl mid mid_prev, ⌜vret = mid↑⌝
-                     ** hd_handler ↦m_h#q_h≻ encode_val Mptr hd
-                     ** tl_handler ↦m_t#q_t≻ encode_val Mptr tl
-                     ** frag_xorlist q mid_prev mid tl Vnullptr (drop idx xs)
-                     ** frag_xorlist q Vnullptr hd mid_prev mid (firstn idx xs))
+                     ** full_xorlist q hd_handler tl_handler xs),
+        (fun vret => ∃ mid mid_prev m_hd_hdl m_tl_hdl p_hd p_tl ofs_hd_hdl ofs_tl_hdl tg_hd_hdl tg_tl_hdl m_hd m_tl i_hd i_tl,
+                     ⌜vret = mid↑⌝
+                     ** hd_handler (↦_m_hd_hdl,q) (encode_val Mptr p_hd)
+                     ** hd_handler (⊨_m_hd_hdl,tg_hd_hdl,q) ofs_hd_hdl
+                     ** ⌜((size_chunk Mptr) | Ptrofs.unsigned ofs_hd_hdl)%Z⌝
+                     ** p_hd (≃_m_hd) i_hd
+                     ** tl_handler (↦_m_tl_hdl,q) (encode_val Mptr p_tl)
+                     ** tl_handler (⊨_m_tl_hdl,tg_tl_hdl,q) ofs_tl_hdl
+                     ** ⌜((size_chunk Mptr) | Ptrofs.unsigned ofs_tl_hdl)%Z⌝
+                     ** p_tl (≃_m_tl) i_tl
+                     ** frag_xorlist q mid_prev mid i_tl Ptrofs.zero (drop idx xs)
+                     ** frag_xorlist q Ptrofs.zero i_hd mid_prev mid (firstn idx xs))
     )))%I.
 
   (* sealed *)
